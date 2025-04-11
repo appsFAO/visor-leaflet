191db157a1e524d56c8afa57e214386c0ad0f69b
appsFAO <grettel.vargasazofeifa@fao.org>
2025-04-11 14:11:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Cargar mapa base
+var map = L.map('map').setView([9.7489, -83.7534], 7);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Cargar capa GeoJSON desde GitHub
+fetch('datos/capa.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data).addTo(map);
+  });
